f8a41a18cc741871c0873e1c40afc865046b40fb
coltronit <coltronit@gmail.com>
2023-12-03 08:03:39 -0800

    committing in progress image manipulation just to switch computers

--- this-or-that-frontend/src/pages/Home.jsx
+++ this-or-that-frontend/src/pages/Home.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom'
+import MainImage from '../components/MainImage'
 
 const Home = () => (
-  <div id='page-wrapper'>
-    <div id='header-container'>
-      <h1>This or That</h1>
-      <h3>Description: </h3>
-      <h4>So this is an app where you like, pick this or that</h4>
+  <>
+    <div id='page-wrapper'>
+      <div id='header-container'>
+        <h1>This or That</h1>
+        <h3>Description: </h3>
+        <h4>So this is an app where you like, pick this or that</h4>
+      </div>
+      <div id='poll-links-container'>
+        <Link id='daily-poll-link' to='/poll'>Daily Poll</Link>
+        <Link id='random-poll-link' to='/poll'>Random Poll</Link>
+        <Link id='past-results-link' to='/results'>Past Results</Link>
+      </div>
     </div>
-    <div id='poll-links-container'>
-      <Link id='daily-poll-link' to='/poll'>Daily Poll</Link>
-      <Link id='random-poll-link' to='/poll'>Random Poll</Link>
-      <Link id='past-results-link' to='/results'>Past Results</Link>
-    </div>
-  </div>
+    <MainImage/>
+  </>
 )
 
 export default Home;
